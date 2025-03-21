e5459eb1bf6278c18acc52ad3903da739b88630c
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-03-21 13:41:51 +0300

    Singly Linked List Node tasarımı gerçekleştirildi

---
+++ Solution/Solution.DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedListNode.cs
@@ -0,0 +1,15 @@
+﻿namespace Solution.DataStructures.LinkedList.SinglyLinkedList
+{
+    public class SinglyLinkedListNode<T>
+    {
+        public SinglyLinkedListNode<T> Next { get; set; }
+        public T Value { get; set; }
+
+        public SinglyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString() => $"{Value}";
+    }
+}
