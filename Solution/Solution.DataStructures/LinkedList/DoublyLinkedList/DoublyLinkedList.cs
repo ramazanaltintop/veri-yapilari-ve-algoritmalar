04604cd637a42f9e3d172ae4786e6d2bbd8851c1
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-03-26 12:33:48 +0300

    Doubly Linked List sınıf tasarımı yapıldı

---
+++ Solution/Solution.DataStructures/LinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,9 @@
+﻿namespace Solution.DataStructures.LinkedList.DoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        public DoublyLinkedListNode<T> Head { get; set; }
+        public DoublyLinkedListNode<T> Tail { get; set; }
+
+    }
+}
