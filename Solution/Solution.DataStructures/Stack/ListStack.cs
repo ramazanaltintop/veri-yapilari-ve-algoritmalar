60667b7f257889a9a0e53f46097d938bcc77b387
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-04-01 19:47:01 +0300

    Stack<T> ana hatlar oluşturuldu

---
+++ Solution/Solution.DataStructures/Stack/ListStack.cs
@@ -0,0 +1,27 @@
+﻿namespace Solution.DataStructures.Stack
+{
+    internal class ListStack<T> : IStack<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solution/Solution.DataStructures/Stack/Stack.cs
@@ -0,0 +1,55 @@
+﻿namespace Solution.DataStructures.Stack
+{
+    public class Stack<T>
+    {
+        private readonly IStack<T> _stack;
+        public int Count => _stack.Count;
+
+        public Stack(StackType type = StackType.List)
+        {
+            if (type == StackType.List)
+            {
+                _stack = new ListStack<T>();
+            }
+            else
+            {
+                _stack = new LinkedListStack<T>();
+            }
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+
+        public T Peek()
+        {
+            return _stack.Peek();
+        }
+
+        public T Pop()
+        {
+            return _stack.Pop();
+        }
+
+        public void Push(T value)
+        {
+            _stack.Push(value);
+        }
+    }
+
+    public interface IStack<T>
+    {
+        int Count { get; }
+        void Push(T item);
+        T Peek();
+        T Pop();
+        void Clear();
+    }
+
+    public enum StackType
+    {
+        List = 0,           // List<T>
+        LinkedList = 1,     // SinglyLinkedList<T>
+    }
+}
