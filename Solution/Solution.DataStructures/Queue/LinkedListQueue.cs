4d513d3d93dd4ede011636990f8e0720c66aa89f
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-04-02 11:22:11 +0300

    Queue uygulaması DoublyLinkedList<T> ile yapıldı

--- Solution/Solution.DataStructures/Queue/LinkedListQueue.cs
+++ Solution/Solution.DataStructures/Queue/LinkedListQueue.cs
@@ -1,22 +1,31 @@
-﻿namespace Solution.DataStructures.Queue
+﻿using Solution.DataStructures.LinkedList.DoublyLinkedList;
+
+namespace Solution.DataStructures.Queue
 {
     public class LinkedListQueue<T> : IQueue<T>
     {
-        public int Count => throw new NotImplementedException();
+        private readonly DoublyLinkedList<T> _list = new DoublyLinkedList<T>();
+        public int Count { get; private set; }
 
         public T DeQueue()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            var temp = _list.RemoveFirst();
+            Count--;
+            return temp;
         }
 
         public void EnQueue(T value)
         {
-            throw new NotImplementedException();
+            if (value is null)
+                throw new ArgumentNullException("Value cannot be null.");
+            _list.AddLast(value);
+            Count++;
         }
 
-        public T Peek()
-        {
-            throw new NotImplementedException();
-        }
+        public T Peek() => Count == 0 
+            ? throw new InvalidOperationException("Queue is empty.") 
+            : _list.Head.Value;
     }
 }
