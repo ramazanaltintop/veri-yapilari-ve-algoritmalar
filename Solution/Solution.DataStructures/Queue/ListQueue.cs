cd1c986ab34267848bb12f65eafb1e2755c31b42
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-04-02 10:57:43 +0300

    Queue<T> ana hatlar oluşturuldu

---
+++ Solution/Solution.DataStructures/Queue/ListQueue.cs
@@ -0,0 +1,22 @@
+﻿namespace Solution.DataStructures.Queue
+{
+    public class ListQueue<T> : IQueue<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public T DeQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnQueue(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solution/Solution.DataStructures/Queue/Queue.cs
@@ -0,0 +1,49 @@
+﻿namespace Solution.DataStructures.Queue
+{
+    public class Queue<T>
+    {
+        private readonly IQueue<T> _queue;
+        public int Count => _queue.Count;
+
+        public Queue(QueueType type = QueueType.List)
+        {
+            if (type == QueueType.List)
+            {
+                _queue = new ListQueue<T>();
+            }
+            else
+            {
+                _queue = new LinkedListQueue<T>();
+            }
+        }
+
+        public T DeQueue()
+        {
+            return _queue.DeQueue();
+        }
+
+        public void EnQueue(T value)
+        {
+            _queue.EnQueue(value);
+        }
+
+        public T Peek()
+        {
+            return _queue.Peek();
+        }
+    }
+
+    public interface IQueue<T>
+    {
+        int Count { get; }
+        void EnQueue(T value);
+        T DeQueue();
+        T Peek();
+    }
+
+    public enum QueueType
+    {
+        List = 0,           // List<T>
+        LinkedList = 1      // DoublyLinkedList<T>
+    }
+}
