bacfb0e1bd53d0f25b8c9ba72821e2819f0a4410
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-04-02 11:21:14 +0300

    Queue uygulaması List<T> ile yapıldı

--- Solution/Solution.DataStructures/Queue/ListQueue.cs
+++ Solution/Solution.DataStructures/Queue/ListQueue.cs
@@ -2,21 +2,32 @@
 {
     public class ListQueue<T> : IQueue<T>
     {
-        public int Count => throw new NotImplementedException();
+        private readonly List<T> _list = new List<T>();
+        public int Count { get; private set; }
 
         public T DeQueue()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            var temp = _list[0];
+            _list.RemoveAt(0);
+            Count--;
+            return temp;
         }
 
         public void EnQueue(T value)
         {
-            throw new NotImplementedException();
+            if (value is null)
+                throw new ArgumentNullException("Value cannot be null.");
+            _list.Add(value);
+            Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+            return _list[0];
         }
     }
 }
