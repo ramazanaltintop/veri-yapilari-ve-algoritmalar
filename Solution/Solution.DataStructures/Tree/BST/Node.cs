012f737d34992f4a5340eeae456436a103390464
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-04-03 15:26:38 +0300

    Binary Search Tree için Node tasarımı gerçekleştirildi

---
+++ Solution/Solution.DataStructures/Tree/BST/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace Solution.DataStructures.Tree.BST
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node()
+        {
+
+        }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
